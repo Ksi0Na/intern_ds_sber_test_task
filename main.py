f951a46d0f4c1f9fab4e6b55f0b77068ce257e5c
Ksi0Na <57129186+Ksi0Na@users.noreply.github.com>
2022-11-13 18:14:31 +0300

    1

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-def data_to_int(data):
-    # delete unused column
+def to_int_data(data):
+    # delete unusable columns
     data.drop('Unnamed: 0', axis=1, inplace=True)
+    data.drop('action_id', axis=1, inplace=True)
+    data.drop('date', axis=1, inplace=True)
 
     # processing NaN values
     data.replace(np.nan, '0', inplace=True)
@@ -12,18 +14,8 @@ def data_to_int(data):
     # processing of the 'person_id' column
     data.iloc[:, 0] = data.iloc[:, 0].str.split('_').str[-1]
 
-    # processing of the 'action_id' column
-    data.iloc[:, 1] = data.iloc[:, 1].str.split('_')
-    data.iloc[:, 1] = data.iloc[:, 1].str[0].str[-1] + data.iloc[:, 1].str[1]
-
-    # processing of the 'date' column
-    data.iloc[:, 2] = data.iloc[:, 2].str.split('-')
-    data.iloc[:, 2] = data.iloc[:, 2].str[0] \
-                      + data.iloc[:, 2].str[1] \
-                      + data.iloc[:, 2].str[2]
-
     # processing of the 'action_type' and 'char_n' columns
-    for i in range(3, len(data.iloc[0, :].index)):
+    for i in range(0, len(data.iloc[0, :].index)):
         data.iloc[:, i] = data.iloc[:, i].str.split().str[-1]
 
     # converting types to int
@@ -32,18 +24,63 @@ def data_to_int(data):
 
     return data
 
+def to_int_person(data):
+    # delete unusable columns
+    data.drop('Unnamed: 0', axis=1, inplace=True)
+    data.drop('date', axis=1, inplace=True)
+    data.drop('group_1', axis=1, inplace=True)
+
+    # processing bool values
+    data.replace('False', '0', inplace=True)
+    data.replace('True', '1', inplace=True)
+
+    # renaming columns
+    data.columns = data.columns.str.replace('har', '')
+
+    # processing of the 'person_id' column
+    data.iloc[:, 0] = data.iloc[:, 0].str.split('_').str[-1]
+
+    # processing of the 'c_n' columns
+    for i in range(0, len(data.iloc[0, :].index) - 1):
+        data.iloc[:, i] = data.iloc[:, i].str.split().str[-1]
+
+    # converting types to int
+    for i in range(0, len(data.iloc[0, :].index)-1):
+        data.iloc[:, i] = pd.to_numeric(data.iloc[:, i])
+
+    return data
+
+def take_concat_data(person,data):
+    l = []
+    for j in range(0, len(person.iloc[:, 0].index) - 1):
+        for i in range(0, len(data.iloc[:, 0].index) - 1):
+            if person.iloc[j, 0] == data.iloc[i, 0]:
+                local = pd.concat([data.iloc[i, :], person.drop('person_id', axis=1).iloc[j, :]], axis=0)
+                local.name = i
+                l.append(local)
+
+    return pd.DataFrame(l)
+
 # reading input files
 train = pd.read_csv("../action_train.csv", dtype=str)
 test = pd.read_csv("../action_test.csv", dtype=str)
+person = pd.read_csv("../person.csv", dtype=str)
+
+# saving and cleaning some data
+y_train = train.iloc[:, -1].astype(int)
+train.drop('result', axis=1, inplace=True)
+action_id = test['action_id']
 
 # data transformation for training
-train = data_to_int(train)
-test = data_to_int(test)
+train = to_int_data(train)
+test = to_int_data(test)
+person = to_int_person(person)
+train = take_concat_data(person, train)
+test = take_concat_data(person, test)
 
 # setting variables for training
 x_train = train.drop('result', axis=1)
 x_test = test
-y_train = train.iloc[:, -1]
 
 # model training
 model = LogisticRegression()
@@ -53,7 +90,7 @@ model.fit(x_train, y_train)
 predict = model.predict_proba(x_test)
 
 # preparing data for output
-output_data = {"action_id": test.iloc[:, 1],
+output_data = {"action_id": action_id,
                "result": predict[:, 1]}
 
 # file creating and output
